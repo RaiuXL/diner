df4a8daec61ae3a08b07bbc8d041c8d3e1c3898c
Nathan Tapia-Ramirez <ntapiaramirez@outlook.com>
2024-04-11 12:21:07 -0400

    Updated index.php and created view controller

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+//328/diner
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+require('vendor/autoload.php');
+
+$F3 = Base::instance();
+
+$F3->route('GET /', function(){
+    $view=new Template();
+    echo $view->render('view/home.html');
+});
+
+$F3->run();
