4cc2a5b4693db9ade8968be5124c0f96ba815605
Nathan Tapia-Ramirez <ntapiaramirez@outlook.com>
2024-04-18 11:36:08 -0400

    Updated Menus and Controller/View to have routes be established

--- index.php
+++ index.php
@@ -12,4 +12,30 @@ $F3->route('GET /', function(){
     echo $view->render('view/home.html');
 });
 
+$F3->route('GET /menus/breakfast', function(){
+    $view=new Template();
+    echo $view->render('view/breakfast-menu.html');
+});
+$F3->route('GET /menus/lunch', function(){
+    $view=new Template();
+    echo $view->render('view/lunch-menu.html');
+});
+$F3->route('GET /menus/dinner', function(){
+    $view=new Template();
+    echo $view->render('view/dinner-menu.html');
+});
+
+
+// Order Form Part One
+$F3->route('GET /order1', function(){
+
+    $view=new Template();
+    echo $view->render('view/order1.html');
+});
+// Order Form Part Two
+$F3->route('GET /order2', function(){
+
+    $view=new Template();
+    echo $view->render('view/order2.html');
+});
 $F3->run();
